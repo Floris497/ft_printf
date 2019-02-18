809d6281fa1584d32cccd7438102b687ede295f3
Floris Fredrikze <floris@noxbox.nl>
2019-02-18 01:53:15 +0100

    Added main for testing

---
+++ ft_printf/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  ft_printf
+//
+//  Created by Floris Fredrikze on 17/02/2019.
+//  Copyright © 2019 Floris Fredrikze. All rights reserved.
+//
+
+#include <stdio.h>
+#include "ft_printf.h"
+
+int main(int argc, const char * argv[]) {
+
+	printf("|");
+	printf("a", 10);
+	printf("|\n");
+
+	return 0;
+}
