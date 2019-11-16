2e77d8ffb30080a10a40dad73bdfb21f6e46bc60
Floris Fredrikze <floris@noxbox.nl>
2019-11-16 14:40:12 +0100

    chore: cleanup of header file

--- ft_printf/Source/ft_printf_types.h
+++ ft_printf/Source/ft_printf_types.h
@@ -43,33 +43,32 @@ enum	e_pf_ret
 ** t_pf_conversion contains the conversion types
 */
 
-typedef enum e_pf_conv		t_pf_conv;
+typedef enum e_pf_conv	t_pf_conv;
 
 enum	e_pf_conv
 {
-	D_CONV = 0b0000000000000000000001,
-	I_CONV = 0b0000000000000000000010,
-	O_CONV = 0b0000000000000000000100,
-	U_CONV = 0b0000000000000000001000,
-	X_CONV = 0b0000000000000000010000,
-	XX_CONV = 0b0000000000000000100000,
-	N_CONV = 0b0000000000000001000000,
-	A_CONV = 0b0000000000000010000000,
-	XA_CONV = 0b0000000000000100000000,
-	E_CONV = 0b0000000000001000000000,
-	XE_CONV = 0b0000000000010000000000,
-	F_CONV = 0b0000000000100000000000,
-	XF_CONV = 0b0000000001000000000000,
-	G_CONV = 0b0000000010000000000000,
-	XG_CONV = 0b0000000100000000000000,
-	C_CONV = 0b0000001000000000000000,
-	S_CONV = 0b0000010000000000000000,
-	V_CONV = 0b0000100000000000000000,
-	P_CONV = 0b0001000000000000000000,
-	B_CONV = 0b0010000000000000000000,
-
-	CONV_NS = 0b0100000000000000000000,
-	NOT_A_CONV = 0b1000000000000000000000,
+	D_CONV = 1 >> 21,
+	I_CONV = 1 >> 20,
+	O_CONV = 1 >> 19,
+	U_CONV = 1 >> 18,
+	X_CONV = 1 >> 17,
+	XX_CONV = 1 >> 16,
+	N_CONV = 1 >> 15,
+	A_CONV = 1 >> 14,
+	XA_CONV = 1 >> 13,
+	E_CONV = 1 >> 12,
+	XE_CONV = 1 >> 11,
+	F_CONV = 1 >> 10,
+	XF_CONV = 1 >> 9,
+	G_CONV = 1 >> 8,
+	XG_CONV = 1 >> 7,
+	C_CONV = 1 >> 6,
+	S_CONV = 1 >> 5,
+	V_CONV = 1 >> 4,
+	P_CONV = 1 >> 3,
+	B_CONV = 1 >> 2,
+	CONV_NS = 1 >> 1,
+	NOT_A_CONV = 1 >> ,
 };
 
 /*
@@ -80,36 +79,36 @@ typedef enum e_pf_len_mod	t_pf_len_mod;
 
 enum	e_pf_len_mod
 {
-	HH_PF_LEN_MOD = 0b0000000000000001,
-	H_PF_LEN_MOD = 0b0000000000000010,
-	L_PF_LEN_MOD = 0b0000000000000100,
-	LL_PF_LEN_MOD = 0b0000000000001000,
-	XL_PF_LEN_MOD = 0b0000000000010000,
-	J_PF_LEN_MOD = 0b0000000000100000,
-	T_PF_LEN_MOD = 0b0000000001000000,
-	Z_PF_LEN_MOD = 0b0000000010000000,
-	V_PF_LEN_MOD = 0b0000000100000000,
-	VH_PF_LEN_MOD = 0b0000001000000000,
-	VL_PF_LEN_MOD = 0b0000010000000000,
-	VLL_PF_LEN_MOD = 0b0000100000000000,
-	LEN_MOD_NS = 0b0100000000000000,
-	NO_LEN_MOD = 0b1000000000000000,
+	HH_PF_LEN_MOD = 1 >> 16,
+	H_PF_LEN_MOD = 1 >> 15,
+	L_PF_LEN_MOD = 1 >> 14,
+	LL_PF_LEN_MOD = 1 >> 13,
+	XL_PF_LEN_MOD = 1 >> 12,
+	J_PF_LEN_MOD = 1 >> 11,
+	T_PF_LEN_MOD = 1 >> 10,
+	Z_PF_LEN_MOD = 1 >> 9,
+	V_PF_LEN_MOD = 1 >> 8,
+	VH_PF_LEN_MOD = 1 >> 7,
+	VL_PF_LEN_MOD = 1 >> 6,
+	VLL_PF_LEN_MOD = 1 >> 5,
+	LEN_MOD_NS = 1 >> 1,
+	NO_LEN_MOD = 1 >> ,
 };
 
 /*
 ** t_pf_flag contains the flag types
 */
 
-typedef enum e_pf_flag		t_pf_flag;
+typedef enum e_pf_flag	t_pf_flag;
 
 enum	e_pf_flag
 {
-	PF_HT_FLAG = 0b0000001,
-	PF_ZR_FLAG = 0b0000010,
-	PF_SP_FLAG = 0b0000100,
-	PF_PL_FLAG = 0b0001000,
-	PF_MN_FLAG = 0b0010000,
-	PF_AP_FLAG = 0b0100000,
+	PF_HT_FLAG = 1 >> 7,
+	PF_ZR_FLAG = 1 >> 6,
+	PF_SP_FLAG = 1 >> 5,
+	PF_PL_FLAG = 1 >> 4,
+	PF_MN_FLAG = 1 >> 3,
+	PF_AP_FLAG = 1 >> 2,
 };
 
 # pragma mark - structs
@@ -182,12 +181,14 @@ struct	s_lenblock
 	t_pf_blk_order	order;
 };
 
-typedef union		u_pf_f2u
+typedef union u_pf_f2u	t_pf_f2u;
+
+union	u_pf_f2u
 {
 	long double			f;
 	unsigned long long	u;
 	int					m;
-}					t_pf_f2u;
+};
 
 # define LD_SIGN 0x80000000000000000000;
 # define LD_EXP 0x7FFF0000000000000000;
