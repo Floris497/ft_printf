c6c72fbc6a950ebea573e89573cff41a20ca6c60
Flavio Miceli <fmiceli@student.codam.nl>
2019-11-19 22:39:22 +0100

    worked on floats, mostly on doing maths on strings.

--- ft_printf/Source/object_printing/ft_printf_print_part_f.c
+++ ft_printf/Source/object_printing/ft_printf_print_part_f.c
@@ -47,30 +47,84 @@ static int		get_dec_exp(int e)
 	return (dec_exp);
 }
 
-static char		*init_str(int sign, int prcs, int d_exp)
+static char		*str_add(char *dst, char *src, size_t n)
 {
-	char	*str;
+	while (n)
+	{
+		dst[n - 1] = dst[n - 1] + (src[n - 1] - '0');
+		if (dst[n - 1] > '9' && n - 2 >= 0)
+			dst[n - 2] += (dst[n - 1] - '0') / 10;
+		n--;
+	}
+	return (dst);
+}
 
-	str = (char*)ft_memmalloc(sizeof(char) * (sign + abs(d_exp) + prcs + 1));
-	if (prcs)
-		str[abs(d_exp) + 1] = '.';
+static char		*set_left_of_dot(char *str, int d_exp, t_ld_parts ld, int *i)
+{
+	int		exp;
+	char	*buff;
+
+	(*i) = 0;
+	buff = (char *)malloc(sizeof(char) * n);
+	exp = (ld.sign_exp & LD_EXP) - LD_EXP_BIAS;
+	while (exp >= 0 && (*i) < LD_MANTISSA_BITS)
+	{
+		exp = (ld.sign_exp & LD_EXP) - LD_EXP_BIAS - (*i);
+		if ((ld.m & (1 << (LD_MANTISSA_BITS - (*i)))))
+		{
+			buff = ft_memset(buff, '0', d_exp);
+			buff[d_exp - 1] = '1';
+			while (exp)
+			{
+				buff = str_add(buff, buff, d_exp);
+				exp--;
+			}
+			str = str_add(str, buff, d_exp);
+		}
+		(*i)++;
+	}
+	free(buff);
 	return (str);
 }
 
-static char		*set_str(char *str, )
+static char		*str_half(char *str, int prcs)
 {
 	int	i;
 
-	i = LD_MANTISSA_BITS;
-	while (i >= 0)
+	i = 0;
+	while (i < prcs)
 	{
-		if (!(ld.m & 1 << i))
-		{
-			i--;
-			continue ;
-		}
-		
+		if ((str[i] - '0') % 2 == 1 && i + 1 < prcs)
+			str[i + 1] = '5';
+		str[i] = ((str[i] - '0') / 2) + '0';
+		i++;
 	}
+	return (buff);
+}
+
+static char		*set_right_of_dot(char *str, int prcs, t_ld_parts ld, int i)
+{
+	int		exp;
+	char	*buff;
+
+	buff = (char *)malloc(sizeof(char) * prcs);
+	while (i < LD_MANTISSA_BITS)
+	{
+		if ((ld.m & (1 << (LD_MANTISSA_BITS - i))))
+		{
+			exp = (ld.sign_exp & LD_EXP) - LD_EXP_BIAS - i;
+			buff = ft_memset(buff, '0', prcs);
+			buff[0] = '5';
+			while (exp < 0)
+			{
+				buff = str_half(buff, prcs);
+				exp++;
+			}
+			str = str_add(str, buff, prcs);
+		}
+		i++;
+	}
+	free(buff);
 	return (str);
 }
 
@@ -79,16 +133,21 @@ t_pf_ret		ft_printf_print_part_f(
 {
 	t_pf_f2u			f2u;
 	int					d_exp;
-	int					e;
+	int					i;
 	char				*str;
+	size_t				size;
 
 	f2u.f = (long double)part->value;
 	if (!(f2u.ld.sign_exp & LD_EXP))
 		return (special_value(f2u.ld));
-	e = (ld.sign_exp & LD_EXP) - LD_EXP_BIAS;
-	d_exp = get_dec_exp(ld, e);
-	str = init_str(ld.sign_exp & LD_SIGN ? TRUE : FALSE, part->prcs, d_exp);
-	str = set_str(str, ld);
+	d_exp = get_dec_exp((ld.sign_exp & LD_EXP) - LD_EXP_BIAS);
+	size = (ld.sign_exp & LD_SIGN ? 1 : 0) + abs(d_exp) + part->prcs + 1;
+	str = (char*)ft_memalloc(sizeof(char) * size);
+	str = ft_memset(str, '0', size - 1);
+	str[abs(d_exp) + 1] = part->prcs ? '.' : '\0';
+	str = set_left_of_dot(str, d_exp, ld, &i);
+	if (part->prcs)
+		str = set_right_of_dot(str[abs(d_exp) + 2], part->prcs, ld, i);
 	ft_putstr(str);
 	free(str);
 	return (PF_RET_SUCCESS);
