cf6a474fc4b39bf290fa2bf8ad59ada3eaa449b9
Floris Fredrikze <floris@noxbox.nl>
2019-12-11 15:33:09 +0100

    fix: for string ignore zero flag when min flag is present

--- ft_printf/Source/object_printing/print_conv_str/pf_print_conv_s.c
+++ ft_printf/Source/object_printing/print_conv_str/pf_print_conv_s.c
@@ -25,7 +25,12 @@ static t_pf_ret	pf_print_pad_conv_s_blk
 		if (lb.order[idx] == 'N')
 			obj->print(str, lb.r_prsc, obj);
 		else if (lb.order[idx] == 'P')
-			pf_print_nchar(' ', lb.pad_len, obj);
+		{
+			if (obj->part->flags & PF_ZR_FLAG)
+				pf_print_nchar('0', lb.pad_len, obj);
+			else
+				pf_print_nchar(' ', lb.pad_len, obj);
+		}
 		idx++;
 	}
 	return (PF_RET_SUCCESS);
