afe0f6c9031432d20594f8de27f8ebce3d69774a
Flavio Miceli <fmiceli@student.codam.nl>
2019-12-11 17:12:10 +0100

    norm: ft_printf_print_part_f.c

--- ft_printf/Source/object_printing/ft_printf_print_part_f.c
+++ ft_printf/Source/object_printing/ft_printf_print_part_f.c
@@ -23,21 +23,19 @@ t_pf_ret	ft_printf_print_part_f(t_pf_obj *obj, t_pf_part *part)
 	idx = 0;
 	obj->part->prcs = (obj->part->prcs == PRCS_NS ? 6 : obj->part->prcs);
 	str = ft_ftoa(part);
-	
-	while (str[idx] != '\0' && (str[idx] == '0' && str[idx + 1] != '.') && !(str[idx] == '0' && str[idx + 1] == '\0')) // is this line dangerouse? can we be sure we can look 1 forward?
+	while (str[idx] != '\0' && (str[idx] == '0' && str[idx + 1] != '.') &&
+	!(str[idx] == '0' && str[idx + 1] == '\0'))
 		idx++;
-	
-	if (str[idx] == '*' && str[idx + 1] == '0') {
+	if (str[idx] == '*' && str[idx + 1] == '0')
+	{
 		free(str);
 		str = malloc(part->prcs + 3);
 		ft_memcpy(str, "0.", 2);
 		ft_memset(str + 2, '0', part->prcs);
 		str[part->prcs + 2] = '\0';
 	}
-	
 	if (str[idx] == '*')
 		part->prcs = 3;
-	
 	pf_print_pad_conv_f(&str[idx], part, obj);
 	free(str);
 	return (PF_RET_SUCCESS);
